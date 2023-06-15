cc258ed52db0f1b1b5e6969409540614ca6c537d
Ahmed Zena <87375061+AhmedZena@users.noreply.github.com>
2023-06-15 22:28:16 +0300

    made contacts

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,64 @@
 import React from "react";
 import "./rightbar.scss";
+
+//search icon
+import { BiSearchAlt } from "react-icons/bi";
+
+// three dots icon
+import { BiDotsHorizontalRounded } from "react-icons/bi";
+
+//import js data
+import { Contacts } from "../../contacts";
+
 function Rightbar() {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="mainContainer">
+        <div className="rightbarTop">
+          <h4 className="rightbarTitle">Contacts</h4>
+          <div className="contactIcons">
+            <BiSearchAlt className="rightbarSearchIcon" />
+            <BiDotsHorizontalRounded className="rightbarDotsIcon" />
+          </div>
+        </div>
+        <div className="rightbarWrapper">
+          <ul className="rightbarFriendList">
+            {Contacts.map((u) => (
+              <li className="rightbarFriend">
+                <div className="imageContainer">
+                  <img
+                    src={u.profilePic}
+                    alt=""
+                    className="rightbarProfileImg"
+                  />
+                  <span
+                    className={
+                      u.lastSeen === "online"
+                        ? "rightbarOnline"
+                        : "rightbarOffline"
+                    }
+                  ></span>
+                </div>
+
+                <div className="namesAndStatus">
+                  <p className="rightbarUsername">{u.name}</p>
+                  <p
+                    className={
+                      u.lastSeen === "online"
+                        ? "rightbarOnlineStatus"
+                        : "rightbarLastSeen"
+                    }
+                  >
+                    {u.lastSeen}
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Rightbar;
---
+++ src/contacts.js
@@ -0,0 +1,106 @@
+export const Contacts = [
+  {
+    id: 1,
+    name: "ahmed-allawy",
+    lastSeen: "online",
+    email: "ahmedallawy23@gmail.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/close-up-portrait-arabian-saudi-sheikh-young-male-model-standing-smiling-looks-happy_155003-23542.jpg?w=360&t=st=1686852865~exp=1686853465~hmac=de558300a2f6035171b5c3bc1449c1b3c1185880488a165bb645bf05033d80bc",
+  },
+  {
+    id: 2,
+    name: "ahmed-gendy",
+    lastSeen: "10 mins ago",
+    email: "ahmedgendy@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1686852913~exp=1686853513~hmac=9f70c75449250ad0591abe6a0f99f6b51362b6a12b4ac6d0bbfe6f9680acd29c",
+  },
+  {
+    id: 3,
+    name: "ahmed-elsayed",
+    lastSeen: "2 mins ago",
+    email: "ahmedelsayed@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1060&t=st=1686853327~exp=1686853927~hmac=1f75bb4b89cf66c3fa498563df735b5d1c936450a438c8188ec35d3ec9a524a6",
+  },
+  {
+    id: 4,
+    name: "ayman-ahmed",
+    lastSeen: "1 hr ago",
+    email: "aymanahmed@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/emotions-people-concept-headshot-serious-looking-handsome-man-with-beard-looking-confident-determined_1258-26730.jpg?w=1060&t=st=1686853355~exp=1686853955~hmac=082d143e6cd60d56cac1514460b52dc144d06dbbd20329b0b52485875a4bcc12",
+  },
+  {
+    id: 5,
+    name: "kamal-shawky",
+    lastSeen: "online",
+    email: "kamalshawky@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/handsome-young-man-with-new-stylish-haircut_176420-19637.jpg?w=1060&t=st=1686853267~exp=1686853867~hmac=1e7eb78d43282ae57fa6af748d571870cd6dbd9f432d67358d92ccb4d2c3cd05",
+  },
+  {
+    id: 6,
+    name: "moaz-ansary",
+    lastSeen: "10 mins ago",
+    email: "moazansary@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/bearded-young-man-wearing-shirt_273609-5938.jpg?w=1060&t=st=1686852998~exp=1686853598~hmac=c996d19b1b1a2eba11929fd36f3c138b876453ef8d8e76c7f18bff6af8ebf3b1",
+  },
+  {
+    id: 7,
+    name: "moaz-elsayed",
+    lastSeen: "2 mins ago",
+    email: "moazelsayed@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/portrait-handsome-serious-man_23-2149022626.jpg?w=1060&t=st=1686853028~exp=1686853628~hmac=4348e84ddcfc3040e327716c34b99a293ac5cac8299d97f9e82459a1edd13ccc",
+  },
+  {
+    id: 8,
+    name: "mohamed-gamal",
+    lastSeen: "1 hr ago",
+    email: "mohamedgamal@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/close-up-bearded-tired-man_176420-18785.jpg?w=1060&t=st=1686853051~exp=1686853651~hmac=18ba123bbee8c6ef377f82f5bc92dde3e9bf98e3825163c5df9f6508ee3fccc3",
+  },
+  {
+    id: 9,
+    name: "mohamed-haitham",
+    lastSeen: "online",
+    email: "mohamedhaitham@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/close-up-portrait-curly-handsome-european-male_176532-8133.jpg?w=1060&t=st=1686853071~exp=1686853671~hmac=cc1cd171d9731e61e7a0b958e5481c5b968bbfb57f495464d52d44cf993e4fc3",
+  },
+  {
+    id: 10,
+    name: "mohamed-reda",
+    lastSeen: "10 mins ago",
+    email: "mohamedreda@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/cheerful-young-man-posing-isolated-grey_171337-10579.jpg?w=1060&t=st=1686853099~exp=1686853699~hmac=52c3cf7bf6b946a29736dc906b53aaf40e6013440aaca7f5acd29555cf47fd64",
+  },
+  {
+    id: 11,
+    name: "mostafa-ahmed",
+    lastSeen: "2 mins ago",
+    email: "mostafaahmed@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/portrait-man-laughing_23-2148859448.jpg?w=740&t=st=1686853146~exp=1686853746~hmac=d5464c0efeb6f1f14d474e340c5140f3248361d75638d931f8c4fdd4e104a585",
+  },
+  {
+    id: 12,
+    name: "osama-saied",
+    lastSeen: "1 hr ago",
+    email: "osamasaied@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/red-haired-serious-young-man-blogger-looks-confidently_273609-16730.jpg?w=1060&t=st=1686853167~exp=1686853767~hmac=8b78b050ed7ae60c7792fd1b934e61469f9af52096252316af57c93d6890bff0",
+  },
+  {
+    id: 13,
+    name: "sayed-mohsen",
+    lastSeen: "online",
+    email: "sayedmohsen@example.com",
+    profilePic:
+      "https://img.freepik.com/free-photo/headshot-handsome-male-freelancer-with-appealing-look-has-dark-beard-mustache-looks-directly-with-serious-look-wears-white-casual-clothes-monochrome-facial-expressions_273609-15456.jpg?w=1060&t=st=1686853192~exp=1686853792~hmac=c4a4d5bd84a4117d275bf881348f523e3637f1dc452d398d518d82b561aaed88",
+  },
+];
