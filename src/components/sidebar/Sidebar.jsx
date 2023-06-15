d213d8b3c0bcc0662e757fb7085df5a4af6ce17a
Ahmed Zena <87375061+AhmedZena@users.noreply.github.com>
2023-06-15 19:30:37 +0300

    enhancement for the navbar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,135 +1,27 @@
 import React from "react";
 import "./sidebar.scss";
+import Items from "./Items";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebarWrapper">
         <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/most-recent.png"}
-              alt="Most Recent"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Most Recent</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/covid.png"}
-              alt="covid-19"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Covid-19 Information</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/groups.png"}
-              alt="Groups"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Groups</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/messenger.png"}
-              alt="Messenger"
-              className="sidebarListIcon"
-            />
-
-            <span className="sidebarListItemText">Messenger</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/watch-videos.png"}
-              alt="Watch Videos"
-              className="sidebarListIcon"
-            />
-
-            <span className="sidebarListItemText">Watch Videos</span>
-          </li>
-
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/market.png"}
-              alt="Market"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Market</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/manage-pages.png"}
-              alt="Manage Pages"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Manage Pages</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/event.png"}
-              alt="Events"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Events</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/fund.png"}
-              alt="Fundraisers"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Fundraisers</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/3d-favourite.png"}
-              alt="Saved"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Saved</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/friends-list.png"}
-              alt="Friends List"
-              className="sidebarListIcon"
-            />
-
-            <span className="sidebarListItemText">Friends List</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/messenger-kids.png"}
-              alt="Messenger Kids"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Messenger Kids</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/games.png"}
-              alt="Games"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Games</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/discount.png"}
-              alt="Discounts"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Discounts</span>
-          </li>
-          <li className="sidebarListItem">
-            <img
-              src={process.env.PUBLIC_URL + "/assets/icons/movies.png"}
-              alt="Movies"
-              className="sidebarListIcon"
-            />
-            <span className="sidebarListItemText">Movies</span>
-          </li>
+          <Items pic="most-recent" text="Most Recent" />
+          <Items pic="covid" text="Covid-19 Information" />
+          <Items pic="groups" text="Groups" />
+          <Items pic="messenger" text="Messenger" />
+          <Items pic="watch-videos" text="Watch Videos" />
+          <Items pic="market" text="Market" />
+          <Items pic="manage-pages" text="Manage Pages" />
+          <Items pic="event" text="Events" />
+          <Items pic="fund" text="Fundraisers" />
+          <Items pic="3d-favourite" text="Saved" />
+          <Items pic="friends-list" text="Friends List" />
+          <Items pic="messenger-kids" text="Messenger Kids" />
+          <Items pic="games" text="Games" />
+          <Items pic="discount" text="Discounts" />
+          <Items pic="movies" text="Movies" />
         </ul>
       </div>
     </div>
